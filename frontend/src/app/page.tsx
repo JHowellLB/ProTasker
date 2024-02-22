edf07b8198df18aabe7c31f8aee3888d70e11110
froilanbuendia <froilangbuendia@gmail.com>
2024-02-22 01:44:15 -0800

    reverted home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-import Navbar from "./components/Navbar/Navbar";
-import { auth } from "./services/firebase";
-import { useRouter } from "next/navigation";
 export default function Home() {
-  const router = useRouter();
-  const googleLogin = () => {
-    auth.onAuthStateChanged(function (user) {
-      if (user) {
-        router.push("/mostused");
-      } else {
-        router.push("/signup");
-      }
-    });
-  };
-  return <section id="home">{googleLogin}</section>;
+  return (
+    <section id="home">
+      <div>Home Page</div>
+    </section>
+  );
 }
