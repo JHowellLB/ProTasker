6ac38bbf5c5aaaaabfd04b5a13f904310587bd8d
froilanbuendia <froilangbuendia@gmail.com>
2024-02-24 17:50:53 -0800

    changed framework to vite+react

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MainCSS from "./main.module.css";
-const MostUsed = () => {
-  return (
-    <section id="main">
-      = <div className={MainCSS.addWebsite}>Add Website</div>
-    </section>
-  );
-};
-export default MostUsed;
