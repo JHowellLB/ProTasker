92e5a05b9b5c6d9e926a677d3effe0d601a08941
froilanbuendia <froilangbuendia@gmail.com>
2024-02-25 23:24:25 -0800

    added storage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
 import MostUsed from "./page/mostused";
 import Visualization from "./page/visualization";
 import SiteLimit from "./page/sitelimit";
 import TaskTimer from "./page/tasktimer";
+import Navbar from "./Navbar/Navbar";
 
 const App = () => {
   return (
