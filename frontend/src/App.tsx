ff7919d9dd9143588bfb44b729cf25cc11016f13
froilanbuendia <froilangbuendia@gmail.com>
2024-02-26 04:37:45 -0800

    added active navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MostUsed from "./page/mostused";
-import Visualization from "./page/visualization";
-import SiteLimit from "./page/sitelimit";
-import TaskTimer from "./page/tasktimer";
-import Navbar from "./Navbar/Navbar";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Tabs from "./components/Tabs/Tabs";
+// import MostUsed from "./page/mostused";
+// import Visualization from "./page/visualization";
+// import SiteLimit from "./page/sitelimit";
+// import TaskTimer from "./page/tasktimer";
+// import Navbar from "./Navbar/Navbar";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<MostUsed />} />
-        <Route path="/visualization" element={<Visualization />} />
-        <Route path="/sitelimit" element={<SiteLimit />} />
-        <Route path="/tasktimer" element={<TaskTimer />} />
-      </Routes>
-    </Router>
+    <div>
+      <Tabs />
+    </div>
   );
 };
 export default App;
