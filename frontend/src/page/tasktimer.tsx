63758df46395176538742b2b1bc6614223aac6e6
Richard Nguyen <richardngocn@gmail.com>
2024-03-09 19:26:24 -0800

    Added an AddTask Button and its Popup Window

--- frontend/src/page/tasktimer.tsx
+++ frontend/src/page/tasktimer.tsx
@@ -1,7 +1,27 @@
+import "./format.css"
+
+const togglePopup = () => {
+    var popup = document.getElementById("popup");
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+  }
+
+
 const TaskTimer = () => {
   return (
     <section>
-      <div>Task Timer</div>
+        <div class="addTask" onClick={() => togglePopup()}>
+             + Add Task
+        </div>
+        <div id="popup" class="popup">
+            <div class="popUpHeader">
+                <h3 class="titleLabel">Add Task</h3>
+                <div class="closeBox" onClick={() => togglePopup()}>X</div>
+            </div>
+        </div>
     </section>
   );
 };
