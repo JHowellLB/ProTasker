c0766b54cc2ca405f6eaa5447f1528a64f6cc149
Richard Nguyen <richardngocn@gmail.com>
2024-03-09 22:38:47 -0800

    Added task button, popup window, input for task name, hours, minutes, and submit button

--- frontend/src/page/tasktimer.tsx
+++ frontend/src/page/tasktimer.tsx
@@ -1,11 +1,11 @@
-import "./format.css"
+import "./task_styles.css"
 
 const togglePopup = () => {
-    var popup = document.getElementById("popup");
-    if (popup.style.display === "none") {
+    let popup = document.getElementById("popup");
+    if (popup?.style.display === "none") {
         popup.style.display = "block";
     } else {
-        popup.style.display = "none";
+        popup!.style.display = "none";
     }
   }
 
@@ -13,13 +13,23 @@ const togglePopup = () => {
 const TaskTimer = () => {
   return (
     <section>
-        <div class="addTask" onClick={() => togglePopup()}>
+        <div className="addTask" onClick={() => togglePopup()}>
              + Add Task
         </div>
-        <div id="popup" class="popup">
-            <div class="popUpHeader">
-                <h3 class="titleLabel">Add Task</h3>
-                <div class="closeBox" onClick={() => togglePopup()}>X</div>
+        <div id="popup" className="popup">
+            <div className="popUpHeader">
+              <h3 className="titleLabel">Add Task</h3>
+              <div className="closeBox" onClick={() => togglePopup()}>X</div>
+            </div>
+            <div>
+              <h3>Task Name:</h3>
+              <input className="task_input" type="text"></input>
+              <h3>Task Timer:</h3>
+              <div className="time_input">
+                <input id="hour" className="time" type="text" placeholder="Hours"></input>
+                <input id="minute" className="time" type="text" placeholder="Minutes"></input>
+                <input className="time" type="submit" value="Save"></input>
+              </div>
             </div>
         </div>
     </section>
