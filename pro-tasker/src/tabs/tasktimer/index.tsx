2feaea9e5fdc51e25cf0af370f0caf4fd71c5d6f
Richard Nguyen <richardngocn@gmail.com>
2024-03-10 22:45:04 -0700

    Changed PopUp Size and Modified Index.tsx

--- pro-tasker/src/tabs/tasktimer/index.tsx
+++ pro-tasker/src/tabs/tasktimer/index.tsx
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 import "./task_styles.css"
-
-const togglePopup = () => {
-    let popup = document.getElementById("popup");
-    if (popup?.style.display === "none") {
-        popup.style.display = "block";
-    } else {
-        popup!.style.display = "none";
-    }
-  }
-
-=======
-import "./tasktimer.css"
 
 const togglePopup = () => {
   let popup = document.getElementById("popup")
@@ -21,37 +8,10 @@ const togglePopup = () => {
     popup!.style.display = "none"
   }
 }
->>>>>>> main
 
 const TaskTimer = () => {
   return (
     <section>
-<<<<<<< HEAD
-        <div className="addTask" onClick={() => togglePopup()}>
-             + Add Task
-        </div>
-        <div id="popup" className="popup">
-            <div className="popUpHeader">
-              <h3 className="titleLabel">Add Task</h3>
-              <div className="closeBox" onClick={() => togglePopup()}>X</div>
-            </div>
-            <div>
-              <h3>Task Name:</h3>
-              <input className="task_input" type="text"></input>
-              <h3>Task Timer:</h3>
-              <div className="time_input">
-                <input id="hour" className="time" type="text" placeholder="Hours"></input>
-                <input id="minute" className="time" type="text" placeholder="Minutes"></input>
-                <input className="time" type="submit" value="Save"></input>
-              </div>
-            </div>
-        </div>
-    </section>
-  );
-};
-
-export default TaskTimer;
-=======
       <div className="addTask" onClick={() => togglePopup()}>
         + Add Task
       </div>
@@ -85,5 +45,4 @@ export default TaskTimer;
   )
 }
 
-export default TaskTimer
->>>>>>> main
+export default TaskTimer 
