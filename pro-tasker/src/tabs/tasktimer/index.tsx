0afbb8e3e8f7a924c38c4f71995fa437562520e7
AriM2001 <Ari.Montes@student.csulb.edu>
2024-03-16 12:14:38 -0700

    Added Edit Popup with Delete Function

--- pro-tasker/src/tabs/tasktimer/index.tsx
+++ pro-tasker/src/tabs/tasktimer/index.tsx
@@ -1,51 +1,45 @@
 import "./task_styles.css"
-import React, {useState} from "react"
+import React, { useState } from 'react';
+
 
 const TaskTimer = () => {
   const [visibility, setVisibility] = useState(false);
+  const [editVisibility, setEditVisibility] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState({id: "", task: "", h: "", m: ""});
 
-const Task = () => {
-  const row = document.createElement("div");
-  row.className = "row";
-  
-  const logger = document.createElement("div");
-  logger.className = "Log";
+  const Task = ({ task, onDelete, onEdit }) => {
+    return (
+      <div className="row" id={task.id}>
+        <div className="Log">
+          <div className="logEle">{task.task}</div>
+          <div className="logEle">{task.h} : {task.m}</div>
+          <button className="PPE">Play/Pause</button>
+        </div>
+        <button className="PPE" onClick={() => { onEdit(task); setEditVisibility(true)}}>Edit</button>
+      </div>
+    );
+  };
 
-  let task = (document.getElementById("task") as HTMLInputElement).value;
-  let h = (document.getElementById("hour") as HTMLInputElement).value;
-  let m = (document.getElementById("minute") as HTMLInputElement).value;
+  const addTask = () => {
+    let task = (document.getElementById("task") as HTMLInputElement).value;
+    let h = (document.getElementById("hour") as HTMLInputElement).value;
+    let m = (document.getElementById("minute")as HTMLInputElement).value;
+    let id = Date.now().toString(); 
+    setTasks([...tasks, {id, task, h, m}]);
+    setVisibility(false);
+  };
 
-  const ltask = document.createElement("div");
-  ltask.innerText = task;
-  ltask.className = "logEle";
-  const ltime = document.createElement("div");
-  ltime.innerText = h + " : " + m;
-  ltime.className = "logEle";
-  const lplay = document.createElement("button");
-  lplay.className = "PPE";
-
-  logger.innerHTML += ltask.outerHTML;
-  logger.innerHTML += ltime.outerHTML;
-  logger.innerHTML += lplay.outerHTML;
-  row.innerHTML += logger.outerHTML;
-
-  const edit = document.createElement("button");
-  edit.className = "PPE";
-
-  row.innerHTML += edit.outerHTML;
-
-  document.getElementById("logBody").innerHTML += row.outerHTML;
-
-  setVisibility(!visibility);
-
-}
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+    setEditVisibility(false);
+  };
 
   return (
     <section>
-      <div className="addTask" onClick={() => setVisibility(!visibility)}>
+      <div className="addTask" onClick={() => setVisibility(true)}>
         + Add Task
       </div>
-
       <div className="taskLog">
         <div className="logHeader">
           <h4>Tasks</h4>
@@ -54,14 +48,15 @@ const Task = () => {
           <h4>Play/Pause</h4>
         </div>
         <div id="logBody">
+          {tasks.map(task => <Task key={task.id} task={task} onDelete={deleteTask} onEdit={setCurrentTask} />)}
         </div>
       </div>
-
+      
       {visibility && (
         <div id="popup" className="popup">
           <div className="popUpHeader">
             <h3 className="titleLabel">Add Task</h3>
-            <div className="closeBox" onClick={() => setVisibility(!visibility)}>
+            <div className="closeBox" onClick={() => setVisibility(false)}>
               X
             </div>
           </div>
@@ -70,23 +65,36 @@ const Task = () => {
             <input id="task" className="task_input" type="text"></input>
             <h3>Task Timer:</h3>
             <div className="time_input">
-              <input
-                id="hour"
-                className="time"
-                type="text"
-                placeholder="Hours"></input>
-              <input
-                id="minute"
-                className="time"
-                type="text"
-                placeholder="Minutes"></input>
-              <input className="time" type="submit" value="Save" onClick={() => Task()}></input>
+              <input id="hour" className="time" type="text" placeholder="Hours"></input>
+              <input id="minute" className="time" type="text" placeholder="Minutes"></input>
+              <input className="time" type="submit" value="Save" onClick={addTask}></input>
+            </div>
+          </div>
+        </div>
+      )}
+      {editVisibility && (
+        <div id="editPopup" className="popup">
+          <div className="popUpHeader">
+            <h3 className="titleLabel">Edit Task</h3>
+            <div className="closeBox" onClick={() => setEditVisibility(false)}>
+              X
+            </div>
+          </div>
+          <div>
+            <h3>Task Name:</h3>
+            <input id="editTask" className="task_input" type="text" value={currentTask.task}></input>
+            <h3>Task Timer:</h3>
+            <div className="time_input">
+              <input id="editHour" className="time" type="text" placeholder="Hours" value={currentTask.h}></input>
+              <input id="editMinute" className="time" type="text" placeholder="Minutes" value={currentTask.m}></input>
+              <input className="time" type="submit" value="Save" onClick={() => {}}></input>
+              <input className="time" type="submit" value="Delete" onClick={() => deleteTask(currentTask.id)}></input>
             </div>
           </div>
         </div>
       )}
     </section>
-  )
-}
+  );
+};
 
-export default TaskTimer 
+export default TaskTimer;
