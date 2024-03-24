84ac61d64d569a1fa4c3c588e9662b15c2a26beb
froilanbuendia <froilangbuendia@gmail.com>
2024-03-24 00:16:25 -0700

    template for timer in background

--- pro-tasker/src/background/index.ts
+++ pro-tasker/src/background/index.ts
@@ -1,2 +1,26 @@
 export {}
-console.log("Background here")
+chrome.alarms.create("taskTimer", {
+  periodInMinutes: 1 / 60
+})
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "taskTimer") {
+    chrome.storage.local.get(["timer", "isRunning"], (res) => {
+      console.log(res.isRunning)
+      if (res.isRunning) {
+        let timer = res.timer + 1
+        console.log(timer)
+        chrome.storage.local.set({
+          timer
+        })
+      }
+    })
+  }
+})
+
+chrome.storage.local.get(["timer", "isRunning"], (res) => {
+  chrome.storage.local.set({
+    timer: "timer" in res ? res.timer : 0,
+    isRunning: "isRunning" in res ? res.isRunning : false
+  })
+})
